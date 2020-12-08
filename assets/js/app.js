581a859d4c776095d0d1dceebbec6408e5b70795
Alejandro Vera Carrasquilla <avera02@misena.edu.co>
2020-12-08 14:13:56 -0500

    javascript fh

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+userSettings: function userSettingsFunction(){
+    let name = 'Alejandro ';
+    let lastName = 'Vera Carrasquilla'
+
+    console.log({name},{lastName});
+}
+
+userSettingsFunction();
